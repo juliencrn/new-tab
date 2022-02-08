41b4d8b4d9e03380d066394cb5f052c99ecfeacd
Julien <juliencaron@protonmail.com>
2022-02-08 05:18:44 +0100

    ship it

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Chrome new tab Extension
+# New tab
 
 Yet another tab chrome extension.
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## About
 
 The story behind this project is pretty simple;
 
-> I want to see my main goal as often as possible using a big, stressful deadline countdown.
+> I want to see my main goal and the remaining days until the deadline as often as possible 🤣
 
-I don't want to deal with user privacy, thus, everything will be stored in the user's browser.
+![demo](./.github/screenshot.png)
+
+
 
 ## Available Scripts
 
@@ -25,9 +25,10 @@ The king piece is the [`manifest.json`](./public/manifest.json).
 
 ### Upload the app to your chrome based browser (locally - dev)
 
-First, build your app using `npm run build`.
-Then, visit `chrome://extensions/` on your chrome browser and enable the developer mode.
-To finish, click on **Load unpacked** and select your `build` folder.
+- Build your app using `npm run build`.
+- Visit `chrome://extensions/` on your chrome browser.
+- Enable the developer mode.
+- Click on **Load unpacked** and select your `build` folder.
 
 Your extension is now loaded and listed on the extension page 🎉.
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useInterval } from 'usehooks-ts'
+import { differenceInDays, format } from 'date-fns'
 
-function App() {
+const DEADLINE = new Date(2023, 8, 18) // 18 sept 2023
+const SECOND = 1000
+
+export default function App() {
+  const [date, setDate] = useState(new Date())
+  let days = differenceInDays(DEADLINE, date)
+
+  useInterval(() => {
+    setDate(new Date())
+  }, SECOND)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='Time'>{format(date, "HH:mm")}</p>
+
+      <pre>
+        {`{`}
+        <br />
+        <div style={{ marginLeft: "2rem" }}>
+          <span className='text-comment'>
+            {`// Days before the $1M deadline`}
+          </span>
+          <br />
+          <span className='text-purple'>let{` `}</span>
+          remaining_days{` `}
+          <span className='text-purple'>={` `}</span>
+          <span className='text-green'>
+            {days > 0 ? days : "null"}
+          </span>
+          {`;`}
+          <br />
+          <br />
+          <span className='text-orange'>lets_fucking_go!()</span>
+          {`;`}
+        </div>
+        {`}`}
+      </pre>
     </div>
   );
 }
-
-export default App;
